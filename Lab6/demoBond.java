d226fd3e3455e1092b973d0a2c1343b264335d06
Jake-Burke <42850452+Jake-Burke@users.noreply.github.com>
2018-10-18 23:38:47 -0400

    Delete demoBond.java

--- Lab6/demoBond.java
+++
@@ -1,7 +0,0 @@
-
-public class demoBond {
-	public static void main(String[] args) {
-		Bond bond1= new Bond(500,100,.05,10000);								//setting numbers to get the price
-		System.out.println("The Price of the bond is: " + bond1.getPrice());	//display price
-	}
-}
